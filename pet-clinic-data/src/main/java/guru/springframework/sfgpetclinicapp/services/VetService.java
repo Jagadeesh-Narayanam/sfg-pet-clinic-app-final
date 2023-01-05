bf15ba2de898ee55ae356167c9f7d2407e3e15a4
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-05 14:17:15 +0530

    refactoring common service interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicapp/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicapp/services/VetService.java
@@ -4,8 +4,6 @@ import guru.springframework.sfgpetclinicapp.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
+
 }
