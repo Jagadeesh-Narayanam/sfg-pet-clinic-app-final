708f93fd7eb8ea89ed437bd79bd43aebf4da2e31
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-05 18:21:06 +0530

    added Vets list and modified controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicapp/controllers/VetController.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinicapp.controllers;
 
+import guru.springframework.sfgpetclinicapp.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/index","vets","vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
