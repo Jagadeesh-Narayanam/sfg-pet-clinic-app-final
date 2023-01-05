39ca2fbbf51ea3d05d4e273c97e814d1edc6c8fb
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-05 15:05:30 +0530

    implementing map based services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicapp/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinicapp.services.map;
+
+import guru.springframework.sfgpetclinicapp.model.Owner;
+import guru.springframework.sfgpetclinicapp.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
