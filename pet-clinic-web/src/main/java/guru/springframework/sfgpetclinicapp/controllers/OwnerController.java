de3977b4f898218ef47a3bf81a553f8bdb6cb876
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-05 18:05:18 +0530

    added owners list and modified controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicapp/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicapp/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package guru.springframework.sfgpetclinicapp.controllers;
 
+import guru.springframework.sfgpetclinicapp.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 
