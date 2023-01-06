10785396d36b197f40f715428cbd39b4608f28f1
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-06 12:38:07 +0530

    added SpecialityServiceMap

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicapp/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicapp.services;
+
+import guru.springframework.sfgpetclinicapp.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
