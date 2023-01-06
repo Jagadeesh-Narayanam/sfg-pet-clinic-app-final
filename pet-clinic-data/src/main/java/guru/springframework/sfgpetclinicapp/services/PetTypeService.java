b986b031ff67ac5803f2aa40075bfe6126f78515
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2023-01-06 11:25:51 +0530

    added PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicapp/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicapp.services;
+
+import guru.springframework.sfgpetclinicapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
